7265f5f51627714f14c97db08f1e196906fa8738
aniket0332 <109891882+aniket0332@users.noreply.github.com>
2023-02-11 19:18:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//server to handle socket io connections
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: "*"
+    }
+})
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+    //  console.log("New user",name)
+        users[socket.id] = name;
+     socket.broadcast.emit('user-joined', name);
+    });
+    socket.on('member-update', use =>{
+        use=users;        
+         socket.emit('memberslist', use);
+        });
+
+    socket.on('send', message =>{
+    socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+    });
+          
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];    
+    });
+    // socket.on('memberemit', users =>{
+    //     socket.broadcast.emit('member', users);
+
+    // });
+})
